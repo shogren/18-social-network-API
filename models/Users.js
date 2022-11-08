01874ed31f43664014b17046018646378c285a4c
jon-shogren <jon.shogren@mailchimp.com>
2022-11-07 19:01:16 -0500

    files moved, models updated

---
+++ models/Users.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// User model created using UserSchema
+const User = model('User', UserSchema);
+
+// export the model
+module.exports = User;
