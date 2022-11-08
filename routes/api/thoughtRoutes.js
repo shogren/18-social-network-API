80314ed4b00a54f7896862f88af3cea37eb7781b
jon-shogren <jon.shogren@mailchimp.com>
2022-11-07 19:00:26 -0500

    routes and models added

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thought-controller');
+
+// GET and POST /api/thoughts
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought);
+
+// GET, PUT, and DELETE /api/thoughts/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// POST /api/thoughts/:thoughtId/reactions
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction);
+
+  router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+module.exports = router;
